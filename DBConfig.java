d8a2b8afdcd99ee76c1ae28d3717e185e96761ec
idb240 <ilian040505@gmail.com>
2025-10-03 23:54:58 +0200

    Add files via upload

--- DBConfig.java
+++ DBConfig.java
@@ -1,9 +1,11 @@
-package Config_db;
-
 public class DBConfig {
 	private String dbpath;
+	private byte pagesize;
+	private int dm_maxfilecount;
 	public DBConfig(String dbpath) {
 		this.dbpath=dbpath;
+		this.pagesize=pagesize;
+		this.dm_maxfilecount=dm_maxfilecount;
 	}
 	public static DBConfig LoadDBConfig(String fichier_config) {
 		DBConfig config = new DBConfig(fichier_config);
