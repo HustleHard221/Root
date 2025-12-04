f5fca7d802ca95a267e3b9f998aadfa6f4ac51fd
VonSweat <89477011+VonSweat@users.noreply.github.com>
2025-12-04 23:25:12 +0100

    Update PageId.java

--- PageId.java
+++ PageId.java
@@ -1,20 +1,29 @@
 import java.util.Objects;
 
+// Classe attribuant un identifiant unique pour une page
 public class PageId {
-    private int FileIdx;
-    private int PageIdx;
+    
+	// Identifiant fichier
+	private int FileIdx;
+    
+	// Rang page dans un fichier
+	private int PageIdx;
 
+	// Constructeur
     public PageId(int fileIdx, int pageIdx) {
         this.FileIdx = fileIdx;
         this.PageIdx = pageIdx;
     }
 
+    // Getters
     public int getFileIdx() { return FileIdx; }
     public int getPageIdx() { return PageIdx; }
 
+    // Gestion affichage
     @Override
     public String toString() { return "PageId(Fichier=" + FileIdx + ", Page=" + PageIdx + ")"; }
 
+    // Comparaison objets PageId
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
